07e26fc0a36dd152e25eadf81b96a658ea10ede8
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-11-22 07:37:57 -0800

    wire redudcer to add payload.data in array

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,11 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+// init state of array
+export default function(state = [], action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      // make a new array, put data inside, flatten
+      return [ action.payload.data, ...state ];
+  }
   return state;
 }
