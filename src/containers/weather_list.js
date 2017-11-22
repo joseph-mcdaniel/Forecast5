e34fda182fe6931d07d7432190f3404b0fd65b17
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-11-22 08:16:47 -0800

    create and render weather_list container

---
+++ src/containers/weather_list.js
@@ -0,0 +1,27 @@
+import React, { Component  } from 'react';
+import { connect } from 'react-redux';
+
+class WeatherList extends Component {
+  render() {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <th>City</th>
+          <th>Temperature</th>
+          <th>Pressure</th>
+          <th>Humidity</th>
+        </thead>
+        <tbody>
+
+        </tbody>
+      </table>
+    );
+  }
+}
+
+function mapStateToProps({ weather }) {
+  // { weather} === { weather: weather }
+  return { weather };
+}
+
+export default connect(mapStateToProps)(WeatherList);
